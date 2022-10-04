2298186b5f3cb4fdad4749d5c5410f1e8303cc39
Aman Singh <singh.aman200226@gmail.com>
2022-10-04 22:02:50 +0530

    i have changed the postions of the button and also some css and added just a minor HTML div

--- app.js
+++ app.js
@@ -2,34 +2,36 @@ let quotesrealdata = "";
 const quotetext = document.querySelector(".quote_area");
 const quoteauthor = document.querySelector(".author");
 quotetext.innerHTML="click on --New Quotes-- to get quotes";
-console.log(quotetext);
+// console.log(quotetext);
 const newquote = () => {
      let rnum = Math.floor(Math.random()*10);
      quotesrealdata = realData[rnum];
     //  console.log(realData[rnum].text);
     //  console.log(realData[rnum].author);
     quotetext.innerHTML =`${quotesrealdata.text}`
-    quoteauthor.innerHTML =`${quotesrealdata.author}`
+    quoteauthor.innerHTML =`~ ${quotesrealdata.author}`
     // console.log(quotetext);
     // console.log(quoteauthor);
 }
 let realData = "";
+
 const getquotes = async () =>{
     const api= "https://type.fit/api/quotes";
     try {
         let data = await fetch(api);
         realData = await data.json();
-        console.log(realData[0]);  
+        // console.log(realData[0]);  
         newquote();
     } catch (error) {
         console.log("error");
     }
 }
+
 const func = document.getElementById('btn');
 
 func.addEventListener("click" , function()
 {
-    document.getElementById("copy-btn").innerHTML = "Copy";
+    document.getElementById("copy-btn").style.boxShadow = "None";;
     getquotes();
 });
 
@@ -42,6 +44,13 @@ function copyQuote()
     //console.log(v);
     navigator.clipboard.writeText(v);
 
-    document.getElementById("copy-btn").innerHTML = "Copied!";
+    document.getElementById("copy-btn").style.boxShadow = " 3px 3px 6px -2px #7e7e7ebf";
 }
+
+function tweetQuote(){
+
+    document.querySelectorAll('.twitter-share-link')[0].href = `https://twitter.com/intent/tweet/?text=${quotetext.innerHTML} ${quoteauthor.innerHTML}`
+}
+
+
 
